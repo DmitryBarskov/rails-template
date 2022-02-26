7315bf4c159c23e2033ade27331f736a30a567a1
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-02-27 00:07:52 +0300

    Mark binstubs as generated

--- templates/security.rb
+++ templates/security.rb
@@ -8,5 +8,10 @@ end
 run_bundle
 
 run "bundle binstubs brakeman bundler-audit"
+
+append_to_file ".gitattributes" do
+  "\nbin/brakeman linguist-generated\nbin/bundler-audit linguist-generated\n"
+end
+
 run "bin/bundler-audit --update"
 run "bin/brakeman"
