6c4aaa2618255923278a031075cc232930f46219
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-03-14 11:29:47 +0300

    Add auto-conversion option

--- templates/slim.rb
+++ templates/slim.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
+views_path = "app/views/"
+question = "Do you want to start auto-conversion of current templates to slim? IT IS UNSAFE!"
+
 gem "slim-rails"
 run_bundle
+
+if yes?(question)
+  gem 'html2slim'
+  run_bundle
+  run "erb2slim -d #{views_path}"
+  run 'bundle remove html2slim'
+  run_bundle
+end
