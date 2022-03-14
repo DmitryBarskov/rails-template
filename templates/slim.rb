2166ae5fa8402a5fdecf2ea254cb949ca4035036
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-03-14 11:38:34 +0300

    Remove vars

--- templates/slim.rb
+++ templates/slim.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
-views_path = "app/views/"
-question = "Do you want to start auto-conversion of current templates to slim? IT IS UNSAFE!"
-
 gem "slim-rails"
 run_bundle
 
-if yes?(question)
-  gem 'html2slim'
+if yes?("Do you want to start auto-conversion of current templates to slim? IT IS UNSAFE!")
+  gem "html2slim"
   run_bundle
-  run "erb2slim -d #{views_path}"
-  run 'bundle remove html2slim'
+  run "erb2slim -d app/views/"
+  run "bundle remove html2slim"
   run_bundle
 end
