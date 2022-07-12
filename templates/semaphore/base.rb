5eb3a46c32ebd23f66bc5893b1f1e1f4a7664abb
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-07-12 20:43:31 +0300

    Add semaphore template

---
+++ templates/semaphore/base.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+def gem_installed?(gem_name)
+  Gem::Specification.find_by_name(gem_name)
+rescue Gem::LoadError
+  nil
+end
+
+file ".semaphore/semaphore.yml", <<~YAML
+  version: v1.0
+  name: Ruby on Rails CI
+  agent:
+    machine:
+      type: e1-standard-2
+      os_image: ubuntu2004
+  auto_cancel:
+    running:
+      when: true
+  fail_fast:
+    stop:
+      when: true
+
+  global_job_config:
+    prologue:
+      commands:
+        - checkout
+        - cache restore
+        #{'- sem-service start postgres' if defined? PG}
+        - sem-version ruby #{RUBY_VERSION}
+        - bundle install
+        - cache store
+
+  blocks:
+    - name: Test
+      task:
+        jobs:
+          - name: Run tests
+            commands:
+              - bundle exec rails db:setup
+              #{'- bundle exec rspec' if gem_installed? 'rspec-rails'}
+
+    - name: Lint
+      task:
+        jobs:
+          - name: Run quality
+            commands:
+              #{'- bundle exec bundler-audit --update' if gem_installed? 'bundler-audit'}
+              #{'- bundle exec brakeman' if gem_installed? 'brakeman'}
+              #{'- bundle exec rubocop  --parallel' if gem_installed? 'rubocop'}
+YAML
