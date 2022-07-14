4edbb023e66cbbe8e82f5b0e9eef3fc2671e3835
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-07-14 21:53:35 +0300

    Apply review comments

--- templates/docker/base.rb
+++ templates/docker/base.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 
+require "yaml"
+
 def source_paths
   [__dir__]
 end
 
 rails_root = File.basename(destination_root)
-additional_packages = if defined? PG
-                        "postgresql-dev"
-                      elsif defined? SQLite3
-                        "sqlite-dev"
-                      else
-                        ""
-                      end
+packages = %w[bash build-base tzdata gcompat]
+packages << "postgresql-dev" if defined? PG
+packages << "sqlite-dev" if defined? SQLite3
 
 file "Dockerfile", <<~DOCKERFILE
   FROM ruby:#{RUBY_VERSION}-alpine
 
   ARG RAILS_ROOT=/#{rails_root}
-  ARG PACKAGES="bash build-base tzdata gcompat #{additional_packages}"
+  ARG PACKAGES="#{packages.join(' ')}"
   ARG BUNDLER_VERSION="#{Bundler::VERSION}"
   ENV BUNDLE_PATH="/bundle_cache"
   ENV GEM_HOME="/bundle_cache"
@@ -40,69 +38,54 @@ file "Dockerfile", <<~DOCKERFILE
   CMD bundle exec rails server -b 0.0.0.0
 DOCKERFILE
 
-if defined? SQLite3
-  file "docker-compose.yml", <<~YAML
-    version: "3.7"
+compose_config = {
+  "version" => "3.7",
+  "services" => {
+    "web" => {
+      "build" => ".",
+      "volumes" => [
+        ".:/#{rails_root}:cached",
+        "bundle_cache:/bundle_cache"
+      ],
+      "ports" => ["3000:3000"],
+      "environment" => {
+        "PIDFILE" => "/tmp/pids/server.pid"
+      },
+      "tmpfs" => [
+        "/tmp/pids/"
+      ]
+    }
+  },
+  "volumes" => {
+    "bundle_cache" => nil,
+    "db_data" => nil
+  }
+}
 
-    services:
-      web:
-        build: .
-        volumes:
-          - .:/#{rails_root}:cached
-          - bundle_cache:/bundle_cache
-          - db_data:/db
-        ports:
-          - 3000:3000
-        environment:
-          PIDFILE: /tmp/pids/server.pid
-        tmpfs:
-          - /tmp/pids/
+compose_config["services"]["web"]["volumes"] << "db_data:/db" if defined? SQLite3
 
-    volumes:
-      bundle_cache:
-      db_data:
-  YAML
-end
 if defined? PG
   db_version = ActiveRecord::Base.connection.select_value("SELECT VERSION()")[/\d+\.\d+/]
 
-  file "docker-compose.yml", <<~YAML
-    version: "3.7"
+  compose_config["services"]["web"]["depends_on"] = ["db"]
+  compose_config["services"]["web"]["environment"]["DATABASE_HOST"] = "db"
+  compose_config["services"]["web"]["environment"]["DATABASE_USERNAME"] = "postgres"
+  compose_config["services"]["web"]["environment"]["DATABASE_PASSWORD"] = "postgres"
 
-    services:
-      web:
-        build: .
-        volumes:
-          - .:/#{rails_root}:cached
-          - bundle_cache:/bundle_cache
-        ports:
-          - 3000:3000
-        depends_on:
-          - db
-        environment:
-          DATABASE_HOST: db
-          DATABASE_USERNAME: postgres
-          DATABASE_PASSWORD: postgres
-          PIDFILE: /tmp/pids/server.pid
-        tmpfs:
-          - /tmp/pids/
+  compose_config["services"]["db"] = {
+    "image" => "postgres:#{db_version}-alpine",
+    "volumes" => [
+      "db_data:/var/lib/postgresql/data/pgdata"
+    ],
+    "ports" => ["5432:5432"],
+    "environment" => {
+      "POSTGRES_USER" => "postgres",
+      "POSTGRES_PASSWORD" => "postgres",
+      "PGDATA" => "/var/lib/postgresql/data/pgdata"
+    }
+  }
 
-      db:
-        image: postgres:#{db_version}-alpine
-        volumes:
-          - db_data:/var/lib/postgresql/data/pgdata
-        ports:
-          - 5432:5432
-        environment:
-          POSTGRES_USER: postgres
-          POSTGRES_PASSWORD: postgres
-          PGDATA: /var/lib/postgresql/data/pgdata
-    volumes:
-      bundle_cache:
-      db_data:
-  YAML
-
-  insert_into_file "config/database.yml", before: "\ndevelopment:" do
+  insert_into_file "config/database.yml", after: /default: &default\n(?: +.+\n)*/ do
     <<~YAML
       \s\susername: <%= ENV["DATABASE_USERNAME"] %>
       \s\spassword: <%= ENV["DATABASE_PASSWORD"] %>
@@ -110,6 +93,8 @@ if defined? PG
     YAML
   end
 end
+
+file "docker-compose.yml", compose_config.to_yaml[4..]
 
 run "docker-compose build"
 run "docker-compose exec web bin/rails db:prepare"
